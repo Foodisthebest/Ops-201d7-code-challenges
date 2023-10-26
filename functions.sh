74eff228fb5442859704c079e978d0b1a70193b5
woodstock <nonaandnea@yahoo.com>
2023-10-26 15:56:47 -0700

    Ops challenge 03 complete!

---
+++ functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script Name:                  Login History
+# Author:                       Your Name
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+login_history () {
+    last | head -3  
+    echo this is the login history
+} # All code lines in the curly brackets are a "codeblock"
+
+
+# Main
+login_history
+
+
+# End
+ 
