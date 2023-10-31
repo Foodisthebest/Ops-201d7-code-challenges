55ebf40ca6090daedc5a5f1fdd1f85aef5fd3009
woodstock <nonaandnea@yahoo.com>
2023-10-30 17:37:32 -0700

    Ops challenge 2 complete!

---
+++ Hello World.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Renona Gay
+# Date of latest revision:      10/24/2023
+# Purpose:                      Ops201 Challenge 02
+
+# Declaration of variables
+greeting="Hello World!"
+perfection="I am working perfectly."
+
+# Declaration of functions
+
+
+# Main
+
+echo $greeting
+echo $perfection
+
+# End
